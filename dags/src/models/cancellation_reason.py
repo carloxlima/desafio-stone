edbb8931dcad7c8a6208ce8f6d742a58f8224997
carloxlima <carllosaugusto@outlook.com>
2025-04-14 02:02:19 -0300

    arquivos deletados

--- dags/src/models/cancellation_reason.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from src.models.base import Base
-
-class CancellationReason(Base):
-    __tablename__ = 'tb_cancellation_reasons'
-    id = Column(Integer, primary_key=True)
-    reason = Column(String, unique=True)
