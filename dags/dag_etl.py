dfcba3b42f3e33cfed42aacc3dd57106d9321a45
carloxlima <carllosaugusto@outlook.com>
2025-04-14 01:53:43 -0300

    arquivo atualizado das dags

--- dags/dag_etl.py
+++ dags/dag_etl.py
@@ -1,14 +1,14 @@
 from airflow.decorators import dag, task
 from airflow.utils.task_group import TaskGroup
 from pendulum import datetime
-from src.services import BucketFileLoader, BucketFileService
-from src.repositories.postgres_writer import PostgresWriter
-import polars as pl
+from src.services import BucketFileLoader, BucketFileService, DataWarehouseETL, VerifyProcessLog, ManagerEvidence
+from src.repositories import PostgresWriterDw, PostgresWriter, PostgresVerifyLog, PostgresManagerEvidence
+
 
 
 
 @dag(
-    dag_id="etl_dag",
+    dag_id="dag_etl_pedrapagamento",
     description="DAG to ETL",
     schedule_interval=None,
     start_date=datetime(2025, 1, 1),
@@ -20,35 +20,90 @@ import polars as pl
 def dat_etl():
 
 
-    with TaskGroup("extractor") as tg_extractor:
+    with TaskGroup("tables_normalizeds") as tg_normalizeds:
+
+        def get_bucket_service():
+            return BucketFileService(PostgresWriter(connection="postgres_conn"))
 
         @task
-        def load_links():   
-            return BucketFileLoader('http://storage.googleapis.com/desafio-eng-dados/').get_files_parquet()
-            
-        @task
-        def load_files(urls):
-            
-            bucket_file_service = BucketFileService(PostgresWriter(connection="postgres_conn"))
-            for url in urls:
-                id = url.split('/')[-1]
-                df = bucket_file_service.insert_file_parquet_from_database(url=url, id=id, table_name='tb_process_log', column_name='file_name')
-                
-            
-        dados_transformados = load_files(load_links())
+        def extractor():
+            pass   
+            return BucketFileLoader('http://storage.googleapis.com/desafio-eng-dados/')\
+                .get_files_parquet()
 
-    with TaskGroup("transformer") as tg_transformer:
+
         @task
-        def create_db():   
+        def transformer(urls):
+            pass
+            bucket_file_service = get_bucket_service()
+            return bucket_file_service.transformation(urls=urls)
+
+        @task
+        def loader(paths):
+            pass
+            i = 0
+            for path in paths:
+                if i == 0:
+                    bucket_file_service = get_bucket_service()
+                    bucket_file_service.loader(path)
+                    bucket_file_service.update_process_log()
+                i += 1
+        @task
+        def load_evidence(arg):
+            lodimg = ManagerEvidence(PostgresManagerEvidence(connection="postgres_conn"))
+            lodimg.manager_evidence(BucketFileLoader('http://storage.googleapis.com/desafio-eng-dados/')\
+                            .get_files_img())
+            
+        dados_transformados = loader(transformer(extractor()))
+        load_evidence(dados_transformados)
+        
+
+
+    @task
+    def check_status():
+        return VerifyProcessLog(PostgresVerifyLog(connection="postgres_conn"))\
+            .check_process_log_complete()
+    # Checagem após a finalização do primeiro TG
+    status_checked = check_status()
+    status_checked.set_upstream(tg_normalizeds)
+
+
+    with TaskGroup("dw") as tg_dw:
+
+        def get_dw_etl():
+            return DataWarehouseETL(PostgresWriterDw(connection="postgres_conn"))
+
+        @task
+        def extractor():   
+            dw = get_dw_etl()
+            result = dw.extract()
+            print(f'EXECUCAO DO EXTRACTOR {result}')
+            return result
+            
+        
+        @task
+        def transformer_and_load_dim(data):
+            dw = get_dw_etl()
+            print(f'DATA RECEBIDA NO TRANSFORMER {data}')
+            dw.transform_and_load_dims(data)
+            return data
+        
+        @task
+        def load_fct(data):
+            dw = get_dw_etl()
+            dw.load_fct(data)
+
+        load_fct(transformer_and_load_dim(extractor()))
+    
+    tg_dw.set_upstream(status_checked)
+
+    with TaskGroup("view") as tg_view:
+        @task
+        def create_dash():   
             pass
         
-        create_db()
+        create_dash()    
+    tg_view.set_upstream(tg_dw)
 
-    with TaskGroup("loader") as tg_loader:
-        @task
-        def create_db():   
-            pass
-        
-        create_db()    
 # Instantiate the DAG
 dat_etl()
--- dags/init/dag_init_create_tables.py
+++ dags/init/dag_init_create_tables.py
@@ -6,17 +6,19 @@ from src.repositories.postgres_create_tables import PostgresCreateTables
 
 @dag(
     dag_id="init_create_tables",
-    schedule_interval=None,
-    start_date=datetime(2025, 1, 1),
+    schedule_interval='@once',
+    start_date=datetime(2024, 1, 1),
     catchup=False,
     default_args={"owner": "Carlos", "retries": 0},
-    tags=["init","new_database"]
+    tags=["init","start_on_boot"]
 )
 
 @task
 def create_tables():
 
-        PostgresCreateTables(connection="postgres_conn").create_tables()
+        creat_tb = PostgresCreateTables(connection="postgres_conn")
+        creat_tb.create_tables_models()
+        print("Tables created successfully.")
     
         
 # Instantiate the DAG
